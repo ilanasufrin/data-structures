84ebda7901cacac5292542c2e17579863661b570
ilanasufrin <ilana.sufrin@huffingtonpost.com>
2014-03-21 03:28:21 -0400

    Create InvalidOperationException.java
    This file was written by the professor.

---
+++ InvalidOperationException.java
@@ -0,0 +1,7 @@
+//this class was written by the professor
+
+
+@SuppressWarnings("serial")
+public class InvalidOperationException extends Exception {
+    public InvalidOperationException(String message){ super(message); }
+}
