d56acf9207ce0acc713f8809df84252c9ad04d19
ilanasufrin <ilana.sufrin@huffingtonpost.com>
2014-03-21 03:38:39 -0400

    Create SortedLinkedSetException.java

---
+++ SortedLinkedSetException.java
@@ -0,0 +1,6 @@
+//this is the professor's file
+
+@SuppressWarnings("serial")
+public class SortedLinkedSetException extends Exception {
+    public SortedLinkedSetException(String message){ super(message); }
+}
